b5ccfc0527aabc5c415a4d691444b8cca3709195
Quros7 <loparev.official@yandex.ru>
2022-11-06 17:42:10 +0500

    made bruteforce script

---
+++ BruteForce_Hash/bruteforce.js
@@ -0,0 +1,26 @@
+// формат запуска скрипта:
+// node bruteforce.js     inp.txt            aba
+// где                  входной файл    искомая строка
+
+const fs = require('fs');
+const { argv } = require("process");
+let inp = argv[2]; // считываем входной файл
+
+let s = fs.readFileSync(inp, "utf8"); // считываем строку, по которой производится поиск
+slen = s.length;
+let t = argv[3] + ""; // считываем искомую подстроку
+tlen = t.length;
+let positions = []; // массив найденный позиций вхождений подстроки в строку
+
+for (let i = 0; i < slen - tlen + 1; i++) {
+    flag = true;
+    for (let j = 0; j < tlen; j++) {
+        if (s[i+j] != t[j]) {
+            flag = false;
+            break;
+        }
+    }
+    if (flag) positions.push(i+1);
+}
+
+console.log(positions);
